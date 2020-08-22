f31d978673c6fb0b192208f65a2d21b6d4b76eeb
dylanfesta <darksnail@gmail.com>
2020-08-22 14:04:18 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.9

---
+++ src/NNSparseCoding.jl
@@ -0,0 +1,5 @@
+module NNSparseCoding
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using NNSparseCoding
+using Test
+
+@testset "NNSparseCoding.jl" begin
+    # Write your tests here.
+end
